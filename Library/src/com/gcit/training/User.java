e4cc2deccde7a189919e046f0dfe569e0b1c1071
Andrei <Dave@Andrei-PC>
2015-07-01 17:46:56 -0400

    Added a new method to display options in the console.

--- Library/src/com/gcit/training/User.java
+++ Library/src/com/gcit/training/User.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class User {
@@ -14,11 +16,59 @@ public class User {
 	protected Connection getConnection() throws SQLException{
 		return DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root","!umug3nI4"); 
 	}
-	public void displayOptions(ArrayList<String> options){
+	public void displayOptions(List<String> options){
 		int s= options.size();
 		for(int i = 1; i<s+1; i++){
 			System.out.println(i+". "+ options.get(i-1));			
 		}		
+	}
+	public int getChoiceNumber(ArrayList<String> options, String cancel){
+		int items = options.size();
+		int pages= items/10;
+		if(pages%10 !=0){
+			pages++;
+		}
+		int curPage=0;
+		int choice=11;
+		boolean f= true;
+		while(f ){
+			int remainder= options.size()-(curPage*10);
+			List<String> displayPage =  options.subList(curPage*10,(curPage*10)+Math.min(10,remainder));
+			displayOptions(displayPage);
+			System.out.println("-----------------------------------");
+			System.out.println((displayPage.size()+1)+". "+ cancel);
+			System.out.println("-----------------------------------");
+			HashMap<Integer, String> actions= new HashMap<Integer,String>();
+			if(curPage>0){				
+				System.out.println((displayPage.size()+2)+". <<<<Previous Page");
+				actions.put((displayPage.size()+2), "prev");
+			}
+			if(curPage<pages-1){
+				System.out.println((displayPage.size()+actions.size()+2)+".Next Page>>>>");
+				actions.put((displayPage.size()+actions.size()+2), "next");
+			}
+			if(actions.size()>0){
+				System.out.println("-----------------------------------");
+				System.out.println("***Page "+(curPage+1) +" of "+pages+"***");
+				System.out.println("-----------------------------------");
+			}
+			choice = getInputInt(1,displayPage.size()+actions.size()+1);			
+			if(choice>0 && choice<displayPage.size()+1){
+				return (curPage*10+choice);
+			}
+			if(choice==displayPage.size()+1){
+				return -1;
+			}
+			else {
+				if(actions.get(choice)=="prev"){
+					curPage--;
+				}
+				else{
+					curPage++;
+				}
+			}
+		}
+		return items;
 	}
 	public int getInputInt(int min, int max){
 		System.out.println("Input choice number ");
@@ -37,11 +87,11 @@ public class User {
 			in = getInputInt(min,max);
 		}
 		System.out.println("****************************************");
-		
+
 		return in;
 	}	
 	//dont share the scanner
-	
+
 	public  String getInputString(){
 		System.out.print(">>");
 		String input= sc.nextLine();
@@ -56,10 +106,10 @@ public class User {
 			input= sc.nextLine();
 		}
 		System.out.println("****************************************");
-		
+
 		return input;
-		
+
 	}
-	
+
 
 }
